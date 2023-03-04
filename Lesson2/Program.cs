01248a897a20b5fb72de3ea3c0d4b2a2fa65aa65
Daniil Ohorodnik <63656944+SnieFox@users.noreply.github.com>
2023-03-04 18:56:28 +0200

    Lesson2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+
+
+//Homework
+//Part 1
+Task1();
+void Task1()
+{
+    Console.WriteLine("Enter x: ");
+    var x = int.TryParse(Console.ReadLine(),out var xResult);
+    Console.WriteLine("Enter y: ");
+    var y = int.TryParse(Console.ReadLine(),out var yResult);
+    if (x & y)
+    {
+        if (xResult == yResult)
+        {
+            Console.WriteLine($"Sum = {xResult}");
+        }
+        else
+        {
+            int sum = 0;
+            for (int i = xResult; i <= yResult; i++)
+            {
+                sum += i;
+                if (i == xResult)
+                {
+                    Console.Write($"{xResult}");
+                }
+                else
+                {
+                    Console.Write($" + {i}");
+                }
+            }
+
+            Console.Write($" = {sum}");
+        }
+
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("Invalid input");
+        //Task1();
+    }
+
+}
